47a896796e2c4ca170c8fe6dfdcf1b91b47c3021
Sakshi03022004 <99466592+Sakshi03022004@users.noreply.github.com>
2023-04-16 11:00:57 +0530

    Add files via upload

---
+++ NEGATIVE.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int no;
+clrscr();
+
+printf("enter the number");
+scanf("%d",&no);
+
+if(no>0)
+{
+printf("number is positive");
+}
+else
+{
+printf("number is negative");
+}
+getch();
+}
