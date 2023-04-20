a73abb1c7313f7b668b376462cc4c4873dd743d1
Sakshi03022004 <99466592+Sakshi03022004@users.noreply.github.com>
2023-04-20 13:12:36 +0530

    Add files via upload

---
+++ ARRAY.C
@@ -0,0 +1,21 @@
+//print array elements
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int seatno[5];//array declaration
+  clrscr();
+  seatno[0]=34;
+  seatno[1]=35;
+  seatno[2]=36;
+  seatno[3]=37;
+  seatno[4]=38;
+
+  printf("%d\n",seatno[0]);
+  printf("%d\n",seatno[1]);
+  printf("%d\n",seatno[2]);
+  printf("%d\n",seatno[3]);
+  printf("%d\n",seatno[4]);
+
+  getch();
+}
